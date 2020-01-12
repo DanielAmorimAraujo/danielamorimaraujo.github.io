f16dc4bbef49950b3cd8d75675a7d5f4fc1069a5
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2020-01-12 00:10:19 -0500

    Updates

---
+++ precache-manifest.295d2cd29676b8b3c733711dc16559a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5000146b3fa4fcab677b44075dbdb6f5",
+    "url": "/DanielAmorimAraujo.github.io/index.html"
+  },
+  {
+    "revision": "1e6e482d9463245768b2",
+    "url": "/DanielAmorimAraujo.github.io/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e912144a843e3c8f2493",
+    "url": "/DanielAmorimAraujo.github.io/static/css/main.f70b66aa.chunk.css"
+  },
+  {
+    "revision": "1e6e482d9463245768b2",
+    "url": "/DanielAmorimAraujo.github.io/static/js/2.4def1f2f.chunk.js"
+  },
+  {
+    "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
+    "url": "/DanielAmorimAraujo.github.io/static/js/2.4def1f2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e912144a843e3c8f2493",
+    "url": "/DanielAmorimAraujo.github.io/static/js/main.a798e235.chunk.js"
+  },
+  {
+    "revision": "805a612e05f16405e797",
+    "url": "/DanielAmorimAraujo.github.io/static/js/runtime-main.d5fc1410.js"
+  },
+  {
+    "revision": "ecb147893f7fed1fd52cff8ac4db52d1",
+    "url": "/DanielAmorimAraujo.github.io/static/media/dropper.ecb14789.png"
+  },
+  {
+    "revision": "fc1102750c23617f4b74b5bfbbd7341a",
+    "url": "/DanielAmorimAraujo.github.io/static/media/elephant.fc110275.gif"
+  },
+  {
+    "revision": "1f2f8a7841f6bd88494b7e67193e8e61",
+    "url": "/DanielAmorimAraujo.github.io/static/media/profile-picture-faded.1f2f8a78.png"
+  },
+  {
+    "revision": "c21831911d26a3ccf5d837c1d5bf3b31",
+    "url": "/DanielAmorimAraujo.github.io/static/media/roameo.c2183191.jpg"
+  }
+]);
