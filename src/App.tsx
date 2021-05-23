4c8f28f5122a06eeb60aa78f7fc9b427920b61c3
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2021-05-23 02:43:09 -0400

    Add particle background and lock scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 
+import Particles from "components/particles/particles";
 import { theme } from "App.styles";
+import { useScrollBlock } from "hooks/useScrollBlock";
 
 const App = (): React.ReactElement => {
+  useScrollBlock()[0]();
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Particles />
     </ThemeProvider>
   );
 };
---
+++ src/components/particles/particles.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ParticlesJS from "react-particles-js";
+
+import { globalColors } from "library/styles";
+
+const Particles = (): React.ReactElement => (
+  <ParticlesJS
+    height="100%"
+    params={{
+      particles: {
+        color: {
+          value: globalColors.text,
+        },
+        number: {
+          value: 320,
+        },
+        size: {
+          value: 2,
+          random: true,
+        },
+        move: {
+          enable: true,
+          direction: "none",
+          random: true,
+          speed: 0.25,
+          straight: false,
+        },
+        opacity: {
+          value: 0.35,
+          random: true,
+        },
+      },
+    }}
+    style={{ position: "absolute", zIndex: -1 }}
+  />
+);
+
+export default Particles;
