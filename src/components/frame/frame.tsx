5bf20f21043b8683eb77f8f99a4d3d451afc92df
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2021-05-25 01:42:47 -0400

    Update frame to take children

--- src/components/frame/frame.tsx
+++ src/components/frame/frame.tsx
@@ -4,24 +4,33 @@ import styled from "styled-components";
 import { globalColors } from "library/styles";
 
 interface IFrame {
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
   angle?: number;
   variant?: "primary" | "secondary";
+  children?: React.ReactElement;
 }
 
 interface IRectangle {
   color: string;
-  width: number;
-  height: number;
+  width: string;
+  height: string;
   angle: number;
 }
 
 const Rectangle = styled.div<IRectangle>`
-  position: absolute;
   background-color: ${(props) => props.color};
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  transform: rotate(${(props) => props.angle}deg);
+`;
+
+const RectangleFront = styled(Rectangle)`
+  position: absolute;
+`;
+
+const ChildrenWrap = styled.div<{ angle: number }>`
+  padding: 16px;
   transform: rotate(${(props) => props.angle}deg);
 `;
 
@@ -30,26 +39,29 @@ const Frame = ({
   height,
   angle,
   variant,
+  children,
 }: IFrame): React.ReactElement => (
   <>
     <Rectangle
       color={
         !variant || variant === "primary" ? globalColors.blue : globalColors.red
       }
-      width={width}
-      height={height}
+      width={width ? width + "px" : "auto"}
+      height={height ? height + "px" : "auto"}
       angle={angle || 10}
-    />
-    <Rectangle
-      color={
-        !variant || variant === "primary"
-          ? globalColors.green
-          : globalColors.pink
-      }
-      width={width}
-      height={height}
-      angle={0}
-    />
+    >
+      <RectangleFront
+        color={
+          !variant || variant === "primary"
+            ? globalColors.green
+            : globalColors.pink
+        }
+        width={width ? width + "px" : "100%"}
+        height={height ? height + "px" : "100%"}
+        angle={angle ? -angle : -10}
+      />
+      <ChildrenWrap angle={angle ? -angle : -10}>{children}</ChildrenWrap>
+    </Rectangle>
   </>
 );
 
