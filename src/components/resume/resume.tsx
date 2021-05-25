47e3a83565c75133750f9e2170e8072298f53d2f
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2021-05-25 01:43:22 -0400

    Add frame to resume and fix sizing

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import styled from "styled-components";
 
+import Frame from "components/frame/frame";
 import ResumePDF from "assets/resume.pdf";
 
+const PdfWrapper = styled.div`
+  canvas {
+    width: auto !important;
+    height: auto !important;
+    max-width: 90vw;
+    max-height: 90vh;
+  }
+`;
+
 const Resume = (): React.ReactElement => (
-  <Document file={ResumePDF}>
-    <Page pageNumber={1} />
-  </Document>
+  <Frame angle={3}>
+    <PdfWrapper>
+      <Document file={ResumePDF}>
+        <Page pageNumber={1} />
+      </Document>
+    </PdfWrapper>
+  </Frame>
 );
 
 export default Resume;
